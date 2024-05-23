4bbe879d53a5a49e12fe0f2c592c27bc2fe2dc8b
gshrutha <tellapadu02@gmail.com>
2024-05-23 15:17:20 +0530

    FirstCommit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebPage.Models;
+
+namespace WebPage.Context
+{
+    public class ApplicationDbContext :DbContext
+    { 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions)
+        :base(contextOptions) 
+        { 
+
+        }
+
+        public DbSet<Customer> TestCustomer1 { get; set; }
+
+
+    }
+}
